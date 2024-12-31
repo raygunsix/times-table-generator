294408d84058070064c00e91ee8f68678e32ab9b
Chris Reid <chris@raygun6.com>
2024-12-31 14:20:15 -0800

    add meme image file

--- generate_multiplication_pdf.py
+++ generate_multiplication_pdf.py
@@ -4,10 +4,13 @@ from fpdf import FPDF
 def generate_multiplication_pdf(base_number, filename="multiplication_practice.pdf"):
     pdf = FPDF()
     pdf.add_page()
-    pdf.set_font("Arial", size=36)
     
-    pdf.cell(200, 10, txt="Multiplication Practice!", ln=True, align='C')
-    pdf.ln(10)
+    # Add image and title in separate columns
+    pdf.set_font("Arial", size=32)
+    pdf.cell(10, 20, txt="", border=0)  # Empty cell for spacing
+    pdf.multi_cell(100, 20, txt="Multiplication Practice!", align='L', border=0)
+    pdf.image("images/math-mental-abuse.jpg", x=120, y=10, w=70)
+    pdf.ln(20)
     
     questions_per_column = 10
     column_width = 95
@@ -19,10 +22,11 @@ def generate_multiplication_pdf(base_number, filename="multiplication_practice.p
         multiplier2 = random.randint(0, 12)
         question1 = f"{base_number} x {multiplier1} = "
         question2 = f"{base_number} x {multiplier2} = "
-        
+
+        pdf.ln(10) # Add line spacing      
+        pdf.cell(10, 10, txt="", border=0)  # Empty cell for spacing
         pdf.cell(column_width, 10, txt=question1, ln=0)
         pdf.cell(column_width, 10, txt=question2, ln=1)
-        pdf.ln(10)  # Add double line spacing
     
     pdf.output(filename)
     print(f"PDF generated: {filename}")
